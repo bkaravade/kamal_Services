42d93a25dc6156931668b630e049d3d3a2d8a04d
bkaravade <karawadeballal@gmail.com>
2025-06-16 22:08:43 +0530

    latest changes

--- assets/custom.js
+++ assets/custom.js
@@ -1,20 +1,35 @@
 $(document).ready(function () {
-
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const paramValue = urlParams.get('type'); // Replace 'key' with your query parameter name
+    console.log(paramValue);
     $("#user_div").hide();
     $("#customer_div").hide();
     $("#report_div").hide();
+    $("#payments_div").hide();
+    if(paramValue == "EmpLyy") {
+        $("#user_menu").hide();
+        $("#customer_menu").hide();
+        $("#payments_menu").hide();
+     //   $("#user_menu").hide();
+    }
+    else if(paramValue == "adnim") {}
+    else  {alert("Un-authorised User"); window.location.href='index.html';}
+
 
     $("#dashboard_menu").click(function () {
         $("#dashboard_div").show();
         $("#user_div").hide();
         $("#customer_div").hide();
         $("#report_div").hide();
+        $("#payments_div").hide();
     });
     $("#user_menu").click(function () {
          $("#user_div").show();
         $("#dashboard_div").hide();
         $("#customer_div").hide();
         $("#report_div").hide();
+        $("#payments_div").hide();
     });
     $("#customer_menu").click(function () {
         $("#customer_div").show();
@@ -22,6 +37,7 @@ $(document).ready(function () {
         $("#dashboard_div").hide();
         $("#user_div").hide();
         $("#report_div").hide();
+         $("#payments_div").hide();
     });
     $("#report_menu").click(function () {
         $("#report_div").show();
@@ -29,6 +45,16 @@ $(document).ready(function () {
         $("#dashboard_div").hide();
         $("#customer_div").hide();
         $("#user_div").hide();
+         $("#payments_div").hide();
+    });
+
+     $("#payments_menu").click(function () {
+        $("#report_div").hide();
+
+        $("#dashboard_div").hide();
+        $("#customer_div").hide();
+        $("#user_div").hide();
+         $("#payments_div").show();
     });
 });
 
